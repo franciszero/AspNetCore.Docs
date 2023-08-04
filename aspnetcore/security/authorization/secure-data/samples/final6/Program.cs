fd25ebdca1e0a387d1c76c5a554dcc4c15fe6bdd
Francis Chan <franciszero@gmail.com>
2023-08-04 18:31:28 -0400

    .

--- aspnetcore/security/authorization/secure-data/samples/final6/Program.cs
+++ aspnetcore/security/authorization/secure-data/samples/final6/Program.cs
@@ -22,20 +22,42 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(
     options => options.SignIn.RequireConfirmedAccount = true)
+    // Add Role services to Identity
+    // Append AddRoles to add Role services:
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 #endregion
 
 builder.Services.AddRazorPages();
 
+// Require authenticated users
+// Set the fallback authorization policy to require users to be authenticated
+// Setting the fallback policy is the preferred way to require all users be authenticated.
 builder.Services.AddAuthorization(options =>
 {
+    /**
+     * The fallback authorization policy:
+     *      Is applied to all requests that don't explicitly specify an authorization policy. 
+     *      
+     *      For requests served by endpoint routing, this includes any endpoint that doesn't specify an authorization attribute. 
+     *      For requests served by other middleware after the authorization middleware, such as static files, this applies the policy to all requests.
+     */
     options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        // RequireAuthenticatedUser adds DenyAnonymousAuthorizationRequirement to the current instance, which enforces that the current user is authenticated.
         .RequireAuthenticatedUser()
         .Build();
 });
 #endregion
 
+/**
+ * Services using Entity Framework Core must be registered for dependency injection using AddScoped. 
+ * The ContactIsOwnerAuthorizationHandler uses ASP.NET Core Identity, which is built on Entity Framework Core. 
+ * Register the handlers with the service collection so they're available to the ContactsController through dependency injection. 
+ * Add the following code to the end of ConfigureServices:
+ * 
+ * ContactAdministratorsAuthorizationHandler and ContactManagerAuthorizationHandler are added as singletons. 
+ * They're singletons because they don't use EF and all the information needed is in the Context parameter of the HandleRequirementAsync method.
+ */
 // Authorization handlers.
 builder.Services.AddScoped<IAuthorizationHandler,
                       ContactIsOwnerAuthorizationHandler>();
@@ -60,11 +82,8 @@ using (var scope = app.Services.CreateScope())
     //  dotnet user-secrets set SeedUserPW <pw>
     // option 2:
     //  appsettings.json: "SeedUserPW": "your_seed_user_password_here"
-
-
     var testUserPw = builder.Configuration.GetValue<string>("SeedUserPW");
-
-   await SeedData.Initialize(services, testUserPw);
+    await SeedData.Initialize(services, testUserPw);
 }
 #endregion
 
